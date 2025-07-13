37b44775103df345522039c313a2e3dbc58f5e55
Paweł Oczadły <p@oczadly.io>
2025-07-13 08:14:02 +0200

    feat: initSpringBootProject task reuses and invalidates cache properly
    * Adds @CacheableTask annotation to initSpringBootProjectTask.
    * Includes functional tests covering incremental build support and build
      cache.

--- plugin/src/functionalTest/groovy/io/oczadly/tasks/InitSpringBootProjectTaskCacheFunctionalSpec.groovy
+++ plugin/src/functionalTest/groovy/io/oczadly/tasks/InitSpringBootProjectTaskCacheFunctionalSpec.groovy
@@ -13,6 +13,9 @@ class InitSpringBootProjectTaskCacheFunctionalSpec extends Specification {
     @TempDir
     private File testProjectDir
 
+    @TempDir
+    private File testCacheDir
+
     private final String initSpringBootProjectTaskName = PluginConfig.getOrThrow PluginConstants.TASK_INITSPRINGBOOTPROJECT_NAME
     private final String outputDir = PluginConfig.getOrThrow PluginConstants.TASK_INITSPRINGBOOTPROJECT_PROPERTY_OUTPUTDIR_DEFAULT
     private final String zipName = PluginConfig.getOrThrow PluginConstants.TASK_INITSPRINGBOOTPROJECT_PROPERTY_ZIPFILE_DEFAULT
@@ -58,5 +61,84 @@ plugins {
 
         then:
         ZipTestUtils.projectFilesExist unzipDir, 'build.gradle', 'src/main/java/com/example/demo/DemoApplication.java'
+    }
+
+    def 'initSpringBootProject supports build cache'() {
+        given:
+        def generatedProjectDir = new File(testProjectDir, outputDir)
+        def zipFile = new File(generatedProjectDir, zipName)
+        def args = GradleTestRunner.asListOfStrings([
+                initSpringBootProjectTaskName,
+                '-PprojectType=maven-project',
+                '-Planguage=java',
+                "-PoutputDir=${generatedProjectDir.absolutePath}",
+                '--configuration-cache',
+                '--build-cache',
+        ])
+
+        when: 'first run'
+        def firstRun = new GradleTestRunner(projectDir: testProjectDir, gradleUserHomeDir: testCacheDir, args: args).run()
+
+        then:
+        firstRun.task(":$initSpringBootProjectTaskName").outcome == TaskOutcome.SUCCESS
+
+        when: 'delete starter.zip'
+        zipFile.delete()
+
+        and: 'second run'
+        def secondRun = new GradleTestRunner(projectDir: testProjectDir, gradleUserHomeDir: testCacheDir, args: args).run()
+
+        then: 'should reuse cache'
+        secondRun.task(":$initSpringBootProjectTaskName").outcome == TaskOutcome.FROM_CACHE
+    }
+
+    def 'initSpringBootProject supports incremental build'() {
+        given:
+        def args = GradleTestRunner.asListOfStrings([
+                initSpringBootProjectTaskName,
+                '-PprojectType=gradle-project-kotlin',
+                '-Planguage=kotlin',
+                '--configuration-cache',
+        ])
+
+        when: 'first run'
+        def firstRun = new GradleTestRunner(projectDir: testProjectDir, args: args).run()
+
+        then:
+        firstRun.output.contains 'Downloading Spring Boot starter project...'
+
+        and:
+        firstRun.task(":$initSpringBootProjectTaskName").outcome == TaskOutcome.SUCCESS
+
+        when: 'second run with the same inputs'
+        def secondRun = new GradleTestRunner(projectDir: testProjectDir, args: args).run()
+
+        then:
+        secondRun.task(":$initSpringBootProjectTaskName").outcome == TaskOutcome.UP_TO_DATE
+    }
+
+    def 'initSpringBootProject is not FROM_CACHE when inputs change'() {
+        given:
+        def generatedProjectDir = new File(testProjectDir, outputDir)
+        def baseArgs = GradleTestRunner.asListOfStrings([
+                initSpringBootProjectTaskName,
+                "-PoutputDir=${generatedProjectDir.absolutePath}",
+                '--configuration-cache',
+                '--build-cache',
+        ])
+        def firstArgs = baseArgs
+        def secondArgs = baseArgs + ['-PprojectType=gradle-project-kotlin', '-Planguage=kotlin']
+
+        when: 'first run'
+        def firstRun = new GradleTestRunner(projectDir: testProjectDir, gradleUserHomeDir: testCacheDir, args: firstArgs).run()
+
+        then:
+        firstRun.task(":$initSpringBootProjectTaskName").outcome == TaskOutcome.SUCCESS
+
+        when: 'second run'
+        def secondRun = new GradleTestRunner(projectDir: testProjectDir, gradleUserHomeDir: testCacheDir, args: secondArgs).run()
+
+        then:
+        secondRun.task(":$initSpringBootProjectTaskName").outcome == TaskOutcome.SUCCESS
     }
 }
--- plugin/src/main/groovy/io/oczadly/tasks/InitSpringBootProjectTask.groovy
+++ plugin/src/main/groovy/io/oczadly/tasks/InitSpringBootProjectTask.groovy
@@ -8,11 +8,13 @@ import io.oczadly.internal.utils.UrlUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
+@CacheableTask
 abstract class InitSpringBootProjectTask extends DefaultTask {
 
     @Input
